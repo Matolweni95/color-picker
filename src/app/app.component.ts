a69a459711376807ab98c020486ceb5682397d4e
masixolematolweni.1@gmail.com <72172920+Matolweni95@users.noreply.github.com>
2022-01-27 13:13:22 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import {HostListener} from '@angular/core';
 
+@HostListener('click', ['$event.target'])
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'color-picker';
+  color: any;
+  test:any;
+  colors:any[] = [
+  
+    { value: 'red' , value2: 'Red'},
+    { value: 'orange' , value2: 'Orange'},
+    { value: 'green' , value2: 'Green'},
+    { value: 'purple' , value2: 'Purple'},
+    { value: 'grey' , value2: 'Grey'},
+  
+   
+  ];
+
+  colorChange(color:any){
+    this.test = color;
+    document.getElementById('demo')!.style.backgroundColor = this.test;
+    document.getElementById('demo')!.style.color = "white";
+
+  }
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatCardModule} from '@angular/material/card'; 
+import {MatChipsModule} from '@angular/material/chips'; 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
